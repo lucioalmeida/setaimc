9272c428c3bf28c4001dc92e443d1a4cb1cf6a0d
Lucio <luciocostaalmeida@gmail.com>
2018-02-05 10:46:48 -0200

    initial

---
+++ src/main/java/br/com/seta/setaimc/SetaimcApplication.java
@@ -0,0 +1,12 @@
+package br.com.seta.setaimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetaimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetaimcApplication.class, args);
+	}
+}
